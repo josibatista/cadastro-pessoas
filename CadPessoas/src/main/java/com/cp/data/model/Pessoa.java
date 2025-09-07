31a3ff9b418a41ff42e9afa7adee7a4228b473c1
Pamela Berti Braz <pbraz@alunos.utfpr.edu.br>
2025-09-07 17:09:19 -0300

    feat: validação de nomes em Cidade e Pessoa
    Adicionadas validações @NotBlank e @Size, e @Column(nullable = false) nos atributos 'nome' das entidades Cidade e Pessoa. Isso garante que os nomes não fiquem nulos, vazios ou ultrapassem o limite de caracteres, reforçando a integridade dos dados na aplicação e no banco.

---
+++ CadPessoas/src/main/java/com/cp/data/model/Pessoa.java
@@ -0,0 +1,41 @@
+
+
+
+package com.cp.data.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.io.Serializable;
+
+import static jakarta.persistence.CascadeType.ALL;
+
+/**
+ *
+ * @author utfpr
+ */
+@Entity
+@Data
+public class Pessoa implements Serializable {
+
+    //bla bla bla
+    public Pessoa(){
+        //bla bla bla
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private int id;
+    @NotBlank
+    @Size(max = 100)
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+
+    @ManyToOne(fetch = FetchType.LAZY,cascade = ALL, optional = true)
+    @JoinColumn(name = "cidade")
+  //  @JsonManagedReference
+    private Cidade cidade;
+}
