7b93e785fe56a9d4b058f4b614ab2e8916b341a2
Pamela Berti Braz <pbraz@alunos.utfpr.edu.br>
2025-09-07 21:10:14 -0300

    delete duplicidade CadPessoas directory

--- CadPessoas/src/main/java/com/cp/rest/configuration/CPApplication.java
+++
@@ -1,9 +0,0 @@
-package com.cp.rest.configuration;
-
-import jakarta.ws.rs.core.Application;
-import jakarta.ws.rs.ApplicationPath;
-
-@ApplicationPath("rest")
-public class CPApplication extends Application {
-    
-}
--- CadPessoas/src/main/resources/META-INF/initial-data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO cidade (id, nome) VALUES (1,'Assis');
-INSERT INTO cidade (id, nome) VALUES (2,'Cornelio Procopio');
